1f4e752598c2ee763fa216cf313f2d031f0aed4e
Domenic Scozzafava <domenic.scozzafava@pepperdine.edu>
2023-02-03 12:34:46 -0800

    Create Dependencies (Stage 1 Js)

---
+++ Bonobo.Git.Server/Scripts/DependenciesEdit.js
@@ -0,0 +1,54 @@
+﻿function DepennewField(count) { //dependencyNewField || DependencyNewField
+    var Depentemplate = document.getElementById("Dependencyrow");
+    console.log(Depentemplate);
+    var DepennewDetailRow = Depentemplate.content.cloneNode(true);
+    console.log(DepennewDetailRow);
+
+    Depentemplate.parentNode.appendChild(DepennewDetailRow);
+    var allDetailRows = $(".dependency-details");
+
+    var nextDetailRowIndex = allDetailRows.length - 1;
+
+    if (nextDetailRowIndex >= 0) {
+        setChildNameAndIdIndexes(allDetailRows.last(), "{i}", nextDetailRowIndex + count);
+    }
+}
+function DepenremField(id) {
+    id = id.slice(-1);
+    id = "Dependency_" + id;
+    console.log(id);
+    var account = document.getElementById(id);
+    account.remove();
+    //Fix Id change after delete
+}
+
+function setChildNameAndIdIndexes(element, placeholder, index) {
+    if (element instanceof jQuery) {
+        console.log("It is an element of Jquery")
+        console.log(element)
+        element = element.get(0);
+        console.log("After. ");
+        console.log(element);
+    }
+
+    console.log(element);
+    if (element.children) {
+        console.log("Element has children ")
+        Array.from(element.children).forEach((child) => {
+            if (element.id) {
+                element.id = element.id.replace(placeholder, index);
+            }
+            if (child.id) {
+                child.id = child.id.replace(placeholder, index);
+            }
+            if (child.name) {
+                child.name = child.name.replace(placeholder, index);
+            }
+            if (child.dataset.valmsgFor) {
+                child.dataset.valmsgFor = child.dataset.valmsgFor.replace(placeholder, index);
+            }
+            console.log(index);
+            setChildNameAndIdIndexes(child, placeholder, index);
+        });
+    }
+}
