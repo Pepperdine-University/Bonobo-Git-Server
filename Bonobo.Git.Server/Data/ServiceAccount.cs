caae603745a9134d9217d400535dcb0135b491e9
afegan <afegan@IT-VM-AEGAN>
2023-07-20 12:11:14 -0700

    Removes default Guid from ServiceAccount model

--- Bonobo.Git.Server/Data/ServiceAccount.cs
+++ Bonobo.Git.Server/Data/ServiceAccount.cs
@@ -8,7 +8,7 @@ namespace Bonobo.Git.Server.Data
         public string ServiceAccountName { get; set; }
         public bool InPassManager { get; set; }
         public DateTime? PassLastUpdated { get; set; }
-        public Guid Id { get; set; } = Guid.Parse("94cab580-0018-4a00-9bb4-bed27234ff22");
+        public Guid Id { get; set; }
         public Guid RepositoryId { get; set; }
         public virtual Repository Repository { get; set; }
     }
